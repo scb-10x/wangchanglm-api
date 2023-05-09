6703eda86f6db030d7e7c21e1c6421dff4803264
Surat Teerapittayanon <steerapi@gmail.com>
2023-05-09 14:13:27 +0700

    update gpu config

--- main.py
+++ main.py
@@ -18,41 +18,45 @@ model_name = "pythainlp/wangchanglm-7.5B-sft-enth"
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     return_dict=False,
-    # load_in_8bit=True,
-    # device_map="auto",
+    load_in_8bit=True,
+    device_map="auto",
     # load_in_8bit_fp32_cpu_offload=True,
-    torch_dtype=torch.float32,
-    offload_folder="./",
-    low_cpu_mem_usage=True,
+    torch_dtype=torch.float16,
+    # offload_folder="./",
+    # low_cpu_mem_usage=True,
 )
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
 
 class Input(BaseModel):
     input: str
     max_length: int = 64
-
+    no_repeat_ngram_size: int = 2
+    top_k: int = 50
+    top_p: float = 0.95
+    typical_p: float = 1.
+    temperature: float = 0.9
 
 @app.post("/generate")
 def generate(input: Input):
     batch = tokenizer(input.input, return_tensors="pt")
-    output_tokens = model.generate(
-        input_ids=batch["input_ids"],
-        max_new_tokens=input.max_length,  # 512
-        begin_suppress_tokens=[],
-        no_repeat_ngram_size=2,
+    with torch.cuda.amp.autocast(): 
+        output_tokens = model.generate(
+            input_ids=batch["input_ids"],
+            max_new_tokens=input.max_length,  # 512
+            begin_suppress_tokens=[],
+            no_repeat_ngram_size=input.no_repeat_ngram_size,
 
-        # oasst k50
-        top_k=50,
-        top_p=0.95,  # 0.95
-        typical_p=1.,
-        temperature=0.9,  # 0.9
+            # oasst k50
+            top_k=input.top_k,
+            top_p=input.top_p,  # 0.95
+            typical_p=input.typical_p,
+            temperature=input.temperature,  # 0.9
 
-        # #oasst typical3
-        # typical_p = 0.3,
-        # temperature = 0.8,
-        # repetition_penalty = 1.2,
-    )
+            # #oasst typical3
+            # typical_p = 0.3,
+            # temperature = 0.8,
+            # repetition_penalty = 1.2,
+        )
     output = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
     # output = pipe(f"{input.input}", max_length=input.max_length)
     return {"output": output}
