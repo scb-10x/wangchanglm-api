624de42fb334e4684a1eb437e73c483fe6f2d584
Kasima Tharnpipitchai <kasima@gmail.com>
2023-05-10 17:10:04 +0700

    Support context parameter

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from pydantic import BaseModel
 
 import torch
 
+PROMPT_FORMATS = {
+    "with_context": (
+        "<context>: {context}\n<human>: {instruction}\n<bot>: "
+    ),
+    "no_context": (
+        "<human>: {instruction}\n<bot>: "
+    ),
+}
+
 app = FastAPI()
 
 # pipe = pipeline("text-generation", model="facebook/xglm-7.5B", revision="sharded")
@@ -15,6 +24,7 @@ app = FastAPI()
 #                 offload_folder="./",
 #                 low_cpu_mem_usage=True,)
 model_name = "pythainlp/wangchanglm-7.5B-sft-enth"
+# model_name = "facebook/xglm-564M"
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     return_dict=False,
@@ -27,8 +37,9 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-class Input(BaseModel):
-    input: str
+class GenerateParams(BaseModel):
+    instruction: str
+    context: str = ""
     max_length: int = 64
     no_repeat_ngram_size: int = 2
     top_k: int = 50
@@ -36,22 +47,48 @@ class Input(BaseModel):
     typical_p: float = 1.
     temperature: float = 0.9
 
+def format_prompt(params: GenerateParams):
+    """
+    Generate a formatted prompt string based on the given parameters.
+
+    Args:
+        params (GenerateParams): A named tuple containing at least the following fields:
+            - instruction (str): The instruction to display in the prompt.
+            - context (str): The context to display in the prompt, if any.
+
+    Returns:
+        str: The formatted prompt string.
+    """
+    if params.context == 'none' or len(params.context) < 2:
+        prompt = PROMPT_FORMATS['no_context'].format_map(
+            {'instruction': params.instruction, 'context': ''})
+    else:
+        prompt = PROMPT_FORMATS['prompt_input'].format_map(
+            {'instruction': params.instruction, 'context': params.context})
+    return prompt
+
+
+### ROUTES ###
+
 @app.post("/generate")
-def generate(input: Input):
-    batch = tokenizer(input.input, return_tensors="pt")
+def generate(params: GenerateParams):
+    prompt = format_prompt(params)
+    # print(f"Prompt: {prompt}")
+
+    batch = tokenizer(prompt, return_tensors="pt")
     input_ids = batch["input_ids"].to('cuda')
     with torch.cuda.amp.autocast(): 
         output_tokens = model.generate(
             input_ids=input_ids,
-            max_new_tokens=input.max_length,  # 512
+            max_new_tokens=params.max_length,  # 512
             begin_suppress_tokens=[],
-            no_repeat_ngram_size=input.no_repeat_ngram_size,
+            no_repeat_ngram_size=params.no_repeat_ngram_size,
 
             # oasst k50
-            top_k=input.top_k,
-            top_p=input.top_p,  # 0.95
-            typical_p=input.typical_p,
-            temperature=input.temperature,  # 0.9
+            top_k=params.top_k,
+            top_p=params.top_p,  # 0.95
+            typical_p=params.typical_p,
+            temperature=params.temperature,  # 0.9
 
             # #oasst typical3
             # typical_p = 0.3,
